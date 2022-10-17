27009069e0e170ade015fb1716cf1c480899ead6
Yannick Ulrich <yannick.ulrich@durham.ac.uk>
2022-10-17 20:20:56 +0100

    Pass filesystem

--- src/displayapp/DisplayApp.cpp
+++ src/displayapp/DisplayApp.cpp
@@ -456,7 +456,7 @@ void DisplayApp::LoadApp(Apps app, DisplayApp::FullRefreshDirections direction) 
       currentScreen = std::make_unique<Screens::Twos>(this);
       break;
     case Apps::ImageView:
-      currentScreen = std::make_unique<Screens::ImageView>(this);
+      currentScreen = std::make_unique<Screens::ImageView>(this, filesystem);
       break;
     case Apps::Paint:
       currentScreen = std::make_unique<Screens::InfiniPaint>(this, lvgl, motorController);
